0b5ef94d44045e44937ed49a4ab84fd8f16246e3
spartawwy <32713201+spartawwy@users.noreply.github.com>
2018-05-29 18:45:57 +0800

    edit for release bug
    find i't need exchbase.kd

---
+++ TradeWinner/GeneratedFiles/Release/moc_winner_app.cpp
@@ -0,0 +1,249 @@
+/****************************************************************************
+** Meta object code from reading C++ file 'winner_app.h'
+**
+** Created by: The Qt Meta Object Compiler version 67 (Qt 5.2.1)
+**
+** WARNING! All changes made in this file will be lost!
+*****************************************************************************/
+
+#include "../../winner_app.h"
+#include <QtCore/qbytearray.h>
+#include <QtCore/qmetatype.h>
+#if !defined(Q_MOC_OUTPUT_REVISION)
+#error "The header file 'winner_app.h' doesn't include <QObject>."
+#elif Q_MOC_OUTPUT_REVISION != 67
+#error "This file was generated using the moc from 5.2.1. It"
+#error "cannot be used with the include files from this version of Qt."
+#error "(The moc has changed too much.)"
+#endif
+
+QT_BEGIN_MOC_NAMESPACE
+struct qt_meta_stringdata_WinnerApp_t {
+    QByteArrayData data[17];
+    char stringdata[225];
+};
+#define QT_MOC_LITERAL(idx, ofs, len) \
+    Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
+    offsetof(qt_meta_stringdata_WinnerApp_t, stringdata) + ofs \
+        - idx * sizeof(QByteArrayData) \
+    )
+static const qt_meta_stringdata_WinnerApp_t qt_meta_stringdata_WinnerApp = {
+    {
+QT_MOC_LITERAL(0, 0, 9),
+QT_MOC_LITERAL(1, 10, 17),
+QT_MOC_LITERAL(2, 28, 0),
+QT_MOC_LITERAL(3, 29, 13),
+QT_MOC_LITERAL(4, 43, 12),
+QT_MOC_LITERAL(5, 56, 5),
+QT_MOC_LITERAL(6, 62, 13),
+QT_MOC_LITERAL(7, 76, 9),
+QT_MOC_LITERAL(8, 86, 12),
+QT_MOC_LITERAL(9, 99, 13),
+QT_MOC_LITERAL(10, 113, 20),
+QT_MOC_LITERAL(11, 134, 16),
+QT_MOC_LITERAL(12, 151, 22),
+QT_MOC_LITERAL(13, 174, 13),
+QT_MOC_LITERAL(14, 188, 8),
+QT_MOC_LITERAL(15, 197, 13),
+QT_MOC_LITERAL(16, 211, 12)
+    },
+    "WinnerApp\0SigTaskStatChange\0\0StrategyTask*\0"
+    "SigAppendLog\0char*\0SigRemoveTask\0"
+    "SigShowUi\0std::string*\0SigShowLongUi\0"
+    "SigEnableBtnBackTest\0SlotStopAllTasks\0"
+    "DoStrategyTasksTimeout\0DoNormalTimer\0"
+    "DoShowUi\0flash_taskbar\0DoShowLongUi\0"
+};
+#undef QT_MOC_LITERAL
+
+static const uint qt_meta_data_WinnerApp[] = {
+
+ // content:
+       7,       // revision
+       0,       // classname
+       0,    0, // classinfo
+      13,   14, // methods
+       0,    0, // properties
+       0,    0, // enums/sets
+       0,    0, // constructors
+       0,       // flags
+       6,       // signalCount
+
+ // signals: name, argc, parameters, tag, flags
+       1,    2,   79,    2, 0x06,
+       4,    1,   84,    2, 0x06,
+       6,    1,   87,    2, 0x06,
+       7,    2,   90,    2, 0x06,
+       9,    2,   95,    2, 0x06,
+      10,    0,  100,    2, 0x06,
+
+ // slots: name, argc, parameters, tag, flags
+      11,    1,  101,    2, 0x0a,
+      12,    0,  104,    2, 0x08,
+      13,    0,  105,    2, 0x08,
+      14,    2,  106,    2, 0x08,
+      14,    1,  111,    2, 0x28,
+      16,    2,  114,    2, 0x08,
+      16,    1,  119,    2, 0x28,
+
+ // signals: parameters
+    QMetaType::Void, 0x80000000 | 3, QMetaType::Int,    2,    2,
+    QMetaType::Void, 0x80000000 | 5,    2,
+    QMetaType::Void, QMetaType::Int,    2,
+    QMetaType::Void, 0x80000000 | 8, QMetaType::Bool,    2,    2,
+    QMetaType::Void, 0x80000000 | 8, QMetaType::Bool,    2,    2,
+    QMetaType::Void,
+
+ // slots: parameters
+    QMetaType::Void, QMetaType::Bool,    2,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void, 0x80000000 | 8, QMetaType::Bool,    2,   15,
+    QMetaType::Void, 0x80000000 | 8,    2,
+    QMetaType::Void, 0x80000000 | 8, QMetaType::Bool,    2,   15,
+    QMetaType::Void, 0x80000000 | 8,    2,
+
+       0        // eod
+};
+
+void WinnerApp::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+{
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        WinnerApp *_t = static_cast<WinnerApp *>(_o);
+        switch (_id) {
+        case 0: _t->SigTaskStatChange((*reinterpret_cast< StrategyTask*(*)>(_a[1])),(*reinterpret_cast< int(*)>(_a[2]))); break;
+        case 1: _t->SigAppendLog((*reinterpret_cast< char*(*)>(_a[1]))); break;
+        case 2: _t->SigRemoveTask((*reinterpret_cast< int(*)>(_a[1]))); break;
+        case 3: _t->SigShowUi((*reinterpret_cast< std::string*(*)>(_a[1])),(*reinterpret_cast< bool(*)>(_a[2]))); break;
+        case 4: _t->SigShowLongUi((*reinterpret_cast< std::string*(*)>(_a[1])),(*reinterpret_cast< bool(*)>(_a[2]))); break;
+        case 5: _t->SigEnableBtnBackTest(); break;
+        case 6: _t->SlotStopAllTasks((*reinterpret_cast< bool(*)>(_a[1]))); break;
+        case 7: _t->DoStrategyTasksTimeout(); break;
+        case 8: _t->DoNormalTimer(); break;
+        case 9: _t->DoShowUi((*reinterpret_cast< std::string*(*)>(_a[1])),(*reinterpret_cast< bool(*)>(_a[2]))); break;
+        case 10: _t->DoShowUi((*reinterpret_cast< std::string*(*)>(_a[1]))); break;
+        case 11: _t->DoShowLongUi((*reinterpret_cast< std::string*(*)>(_a[1])),(*reinterpret_cast< bool(*)>(_a[2]))); break;
+        case 12: _t->DoShowLongUi((*reinterpret_cast< std::string*(*)>(_a[1]))); break;
+        default: ;
+        }
+    } else if (_c == QMetaObject::IndexOfMethod) {
+        int *result = reinterpret_cast<int *>(_a[0]);
+        void **func = reinterpret_cast<void **>(_a[1]);
+        {
+            typedef void (WinnerApp::*_t)(StrategyTask * , int );
+            if (*reinterpret_cast<_t *>(func) == static_cast<_t>(&WinnerApp::SigTaskStatChange)) {
+                *result = 0;
+            }
+        }
+        {
+            typedef void (WinnerApp::*_t)(char * );
+            if (*reinterpret_cast<_t *>(func) == static_cast<_t>(&WinnerApp::SigAppendLog)) {
+                *result = 1;
+            }
+        }
+        {
+            typedef void (WinnerApp::*_t)(int );
+            if (*reinterpret_cast<_t *>(func) == static_cast<_t>(&WinnerApp::SigRemoveTask)) {
+                *result = 2;
+            }
+        }
+        {
+            typedef void (WinnerApp::*_t)(std::string * , bool );
+            if (*reinterpret_cast<_t *>(func) == static_cast<_t>(&WinnerApp::SigShowUi)) {
+                *result = 3;
+            }
+        }
+        {
+            typedef void (WinnerApp::*_t)(std::string * , bool );
+            if (*reinterpret_cast<_t *>(func) == static_cast<_t>(&WinnerApp::SigShowLongUi)) {
+                *result = 4;
+            }
+        }
+        {
+            typedef void (WinnerApp::*_t)();
+            if (*reinterpret_cast<_t *>(func) == static_cast<_t>(&WinnerApp::SigEnableBtnBackTest)) {
+                *result = 5;
+            }
+        }
+    }
+}
+
+const QMetaObject WinnerApp::staticMetaObject = {
+    { &QApplication::staticMetaObject, qt_meta_stringdata_WinnerApp.data,
+      qt_meta_data_WinnerApp,  qt_static_metacall, 0, 0}
+};
+
+
+const QMetaObject *WinnerApp::metaObject() const
+{
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->dynamicMetaObject() : &staticMetaObject;
+}
+
+void *WinnerApp::qt_metacast(const char *_clname)
+{
+    if (!_clname) return 0;
+    if (!strcmp(_clname, qt_meta_stringdata_WinnerApp.stringdata))
+        return static_cast<void*>(const_cast< WinnerApp*>(this));
+    if (!strcmp(_clname, "TSystem::ServerClientAppBase"))
+        return static_cast< TSystem::ServerClientAppBase*>(const_cast< WinnerApp*>(this));
+    return QApplication::qt_metacast(_clname);
+}
+
+int WinnerApp::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = QApplication::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 13)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 13;
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        if (_id < 13)
+            *reinterpret_cast<int*>(_a[0]) = -1;
+        _id -= 13;
+    }
+    return _id;
+}
+
+// SIGNAL 0
+void WinnerApp::SigTaskStatChange(StrategyTask * _t1, int _t2)
+{
+    void *_a[] = { 0, const_cast<void*>(reinterpret_cast<const void*>(&_t1)), const_cast<void*>(reinterpret_cast<const void*>(&_t2)) };
+    QMetaObject::activate(this, &staticMetaObject, 0, _a);
+}
+
+// SIGNAL 1
+void WinnerApp::SigAppendLog(char * _t1)
+{
+    void *_a[] = { 0, const_cast<void*>(reinterpret_cast<const void*>(&_t1)) };
+    QMetaObject::activate(this, &staticMetaObject, 1, _a);
+}
+
+// SIGNAL 2
+void WinnerApp::SigRemoveTask(int _t1)
+{
+    void *_a[] = { 0, const_cast<void*>(reinterpret_cast<const void*>(&_t1)) };
+    QMetaObject::activate(this, &staticMetaObject, 2, _a);
+}
+
+// SIGNAL 3
+void WinnerApp::SigShowUi(std::string * _t1, bool _t2)
+{
+    void *_a[] = { 0, const_cast<void*>(reinterpret_cast<const void*>(&_t1)), const_cast<void*>(reinterpret_cast<const void*>(&_t2)) };
+    QMetaObject::activate(this, &staticMetaObject, 3, _a);
+}
+
+// SIGNAL 4
+void WinnerApp::SigShowLongUi(std::string * _t1, bool _t2)
+{
+    void *_a[] = { 0, const_cast<void*>(reinterpret_cast<const void*>(&_t1)), const_cast<void*>(reinterpret_cast<const void*>(&_t2)) };
+    QMetaObject::activate(this, &staticMetaObject, 4, _a);
+}
+
+// SIGNAL 5
+void WinnerApp::SigEnableBtnBackTest()
+{
+    QMetaObject::activate(this, &staticMetaObject, 5, 0);
+}
+QT_END_MOC_NAMESPACE
---
+++ TradeWinner/GeneratedFiles/Release/moc_winner_win.cpp
@@ -0,0 +1,389 @@
+/****************************************************************************
+** Meta object code from reading C++ file 'winner_win.h'
+**
+** Created by: The Qt Meta Object Compiler version 67 (Qt 5.2.1)
+**
+** WARNING! All changes made in this file will be lost!
+*****************************************************************************/
+
+#include "../../winner_win.h"
+#include <QtCore/qbytearray.h>
+#include <QtCore/qmetatype.h>
+#if !defined(Q_MOC_OUTPUT_REVISION)
+#error "The header file 'winner_win.h' doesn't include <QObject>."
+#elif Q_MOC_OUTPUT_REVISION != 67
+#error "This file was generated using the moc from 5.2.1. It"
+#error "cannot be used with the include files from this version of Qt."
+#error "(The moc has changed too much.)"
+#endif
+
+QT_BEGIN_MOC_NAMESPACE
+struct qt_meta_stringdata_WinnerWin_t {
+    QByteArrayData data[80];
+    char stringdata[1380];
+};
+#define QT_MOC_LITERAL(idx, ofs, len) \
+    Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
+    offsetof(qt_meta_stringdata_WinnerWin_t, stringdata) + ofs \
+        - idx * sizeof(QByteArrayData) \
+    )
+static const qt_meta_stringdata_WinnerWin_t qt_meta_stringdata_WinnerWin = {
+    {
+QT_MOC_LITERAL(0, 0, 9),
+QT_MOC_LITERAL(1, 10, 14),
+QT_MOC_LITERAL(2, 25, 0),
+QT_MOC_LITERAL(3, 26, 13),
+QT_MOC_LITERAL(4, 40, 5),
+QT_MOC_LITERAL(5, 46, 14),
+QT_MOC_LITERAL(6, 61, 23),
+QT_MOC_LITERAL(7, 85, 1),
+QT_MOC_LITERAL(8, 87, 23),
+QT_MOC_LITERAL(9, 111, 22),
+QT_MOC_LITERAL(10, 134, 21),
+QT_MOC_LITERAL(11, 156, 24),
+QT_MOC_LITERAL(12, 181, 14),
+QT_MOC_LITERAL(13, 196, 7),
+QT_MOC_LITERAL(14, 204, 11),
+QT_MOC_LITERAL(15, 216, 11),
+QT_MOC_LITERAL(16, 228, 3),
+QT_MOC_LITERAL(17, 232, 12),
+QT_MOC_LITERAL(18, 245, 9),
+QT_MOC_LITERAL(19, 255, 10),
+QT_MOC_LITERAL(20, 266, 21),
+QT_MOC_LITERAL(21, 288, 3),
+QT_MOC_LITERAL(22, 292, 17),
+QT_MOC_LITERAL(23, 310, 25),
+QT_MOC_LITERAL(24, 336, 8),
+QT_MOC_LITERAL(25, 345, 4),
+QT_MOC_LITERAL(26, 350, 24),
+QT_MOC_LITERAL(27, 375, 16),
+QT_MOC_LITERAL(28, 392, 30),
+QT_MOC_LITERAL(29, 423, 9),
+QT_MOC_LITERAL(30, 433, 22),
+QT_MOC_LITERAL(31, 456, 13),
+QT_MOC_LITERAL(32, 470, 15),
+QT_MOC_LITERAL(33, 486, 21),
+QT_MOC_LITERAL(34, 508, 11),
+QT_MOC_LITERAL(35, 520, 3),
+QT_MOC_LITERAL(36, 524, 18),
+QT_MOC_LITERAL(37, 543, 9),
+QT_MOC_LITERAL(38, 553, 20),
+QT_MOC_LITERAL(39, 574, 24),
+QT_MOC_LITERAL(40, 599, 16),
+QT_MOC_LITERAL(41, 616, 12),
+QT_MOC_LITERAL(42, 629, 16),
+QT_MOC_LITERAL(43, 646, 19),
+QT_MOC_LITERAL(44, 666, 18),
+QT_MOC_LITERAL(45, 685, 22),
+QT_MOC_LITERAL(46, 708, 23),
+QT_MOC_LITERAL(47, 732, 23),
+QT_MOC_LITERAL(48, 756, 28),
+QT_MOC_LITERAL(49, 785, 29),
+QT_MOC_LITERAL(50, 815, 25),
+QT_MOC_LITERAL(51, 841, 19),
+QT_MOC_LITERAL(52, 861, 31),
+QT_MOC_LITERAL(53, 893, 29),
+QT_MOC_LITERAL(54, 923, 28),
+QT_MOC_LITERAL(55, 952, 26),
+QT_MOC_LITERAL(56, 979, 25),
+QT_MOC_LITERAL(57, 1005, 19),
+QT_MOC_LITERAL(58, 1025, 15),
+QT_MOC_LITERAL(59, 1041, 19),
+QT_MOC_LITERAL(60, 1061, 24),
+QT_MOC_LITERAL(61, 1086, 20),
+QT_MOC_LITERAL(62, 1107, 26),
+QT_MOC_LITERAL(63, 1134, 19),
+QT_MOC_LITERAL(64, 1154, 5),
+QT_MOC_LITERAL(65, 1160, 21),
+QT_MOC_LITERAL(66, 1182, 4),
+QT_MOC_LITERAL(67, 1187, 25),
+QT_MOC_LITERAL(68, 1213, 10),
+QT_MOC_LITERAL(69, 1224, 6),
+QT_MOC_LITERAL(70, 1231, 11),
+QT_MOC_LITERAL(71, 1243, 6),
+QT_MOC_LITERAL(72, 1250, 16),
+QT_MOC_LITERAL(73, 1267, 19),
+QT_MOC_LITERAL(74, 1287, 15),
+QT_MOC_LITERAL(75, 1303, 15),
+QT_MOC_LITERAL(76, 1319, 13),
+QT_MOC_LITERAL(77, 1333, 20),
+QT_MOC_LITERAL(78, 1354, 6),
+QT_MOC_LITERAL(79, 1361, 17)
+    },
+    "WinnerWin\0DoQueryCapital\0\0SlotAppendLog\0"
+    "char*\0SlotTabChanged\0SlotTbvTasksContextMenu\0"
+    "p\0SlotTbvTasksActionStart\0"
+    "SlotTbvTasksActionStop\0SlotTbvTasksActionDel\0"
+    "SlotTbvTasksActionDetail\0RemoveByTaskId\0"
+    "task_id\0DoStatusBar\0std::string\0str\0"
+    "std::string*\0is_delete\0DoFlashWin\0"
+    "DoAlertPercentChanged\0val\0DoSellTypeChanged\0"
+    "ChangeTabSellAssistantImg\0TypeTask\0"
+    "type\0DoLeStockEditingFinished\0"
+    "DoLeStockChanged\0DoSpboxQuantityEditingFinished\0"
+    "DoAddTask\0DoTaskStatChangeSignal\0"
+    "StrategyTask*\0DoQueryPosition\0"
+    "InsertIntoTbvTasklist\0QTableView*\0tbv\0"
+    "T_TaskInformation&\0task_info\0"
+    "ResetSellTabTaskTime\0DoBuyAlertPercentChanged\0"
+    "DoBuyTypeChanged\0DoAddBuyTask\0"
+    "DoQueryQtyCanBuy\0ResetBuyTabTaskTime\0"
+    "DoAddEqSectionTask\0ResetEqSectionTaskTime\0"
+    "DoMaxQtyCheckBoxChanged\0DoMinQtyCheckBoxChanged\0"
+    "DoMaxStopTrigCheckBoxChanged\0"
+    "DoMinClearTrigCheckBoxChanged\0"
+    "DoRebounceCheckBoxChanged\0DoAddIndexTradeTask\0"
+    "DoTrdIndexRadioCrossDownChecked\0"
+    "DoTrdIndexRadioCrossUpChecked\0"
+    "DoTrdIndexAlertBtnBtnChecked\0"
+    "DoTrdIndexRelBtnBtnChecked\0"
+    "DoTrdIndexClearBtnChecked\0DoBktestTypeChanged\0"
+    "DoStartBacktest\0DoEnableBtnBackTest\0"
+    "ChangeTabBuyAssistantImg\0TbvTasksCurRowTaskId\0"
+    "FlushFromStationListWidget\0"
+    "OnClickedListWidget\0index\0"
+    "ChangeFromStationText\0text\0"
+    "AssignHintListAndLineEdit\0HintList*&\0"
+    "p_list\0QLineEdit*&\0p_edit\0QDoubleSpinBox*&\0"
+    "p_dbspb_alert_price\0p_dbspb_percent\0"
+    "SlotOpenCalcWin\0SlotOpenAbout\0"
+    "TriggerFlashWinTimer\0enable\0"
+    "DoTabTasksDbClick\0"
+};
+#undef QT_MOC_LITERAL
+
+static const uint qt_meta_data_WinnerWin[] = {
+
+ // content:
+       7,       // revision
+       0,       // classname
+       0,    0, // classinfo
+      56,   14, // methods
+       0,    0, // properties
+       0,    0, // enums/sets
+       0,    0, // constructors
+       0,       // flags
+       0,       // signalCount
+
+ // slots: name, argc, parameters, tag, flags
+       1,    0,  294,    2, 0x0a,
+       3,    1,  295,    2, 0x0a,
+       5,    1,  298,    2, 0x0a,
+       6,    1,  301,    2, 0x0a,
+       8,    1,  304,    2, 0x0a,
+       9,    1,  307,    2, 0x0a,
+      10,    1,  310,    2, 0x0a,
+      11,    1,  313,    2, 0x0a,
+      12,    1,  316,    2, 0x0a,
+      14,    1,  319,    2, 0x0a,
+      14,    2,  322,    2, 0x0a,
+      14,    1,  327,    2, 0x2a,
+      19,    0,  330,    2, 0x0a,
+      20,    1,  331,    2, 0x0a,
+      22,    1,  334,    2, 0x0a,
+      23,    1,  337,    2, 0x0a,
+      26,    0,  340,    2, 0x0a,
+      27,    1,  341,    2, 0x0a,
+      28,    0,  344,    2, 0x0a,
+      29,    0,  345,    2, 0x0a,
+      30,    2,  346,    2, 0x0a,
+      32,    0,  351,    2, 0x0a,
+      33,    2,  352,    2, 0x0a,
+      38,    0,  357,    2, 0x0a,
+      39,    1,  358,    2, 0x0a,
+      40,    1,  361,    2, 0x0a,
+      41,    0,  364,    2, 0x0a,
+      42,    0,  365,    2, 0x0a,
+      43,    0,  366,    2, 0x0a,
+      44,    0,  367,    2, 0x0a,
+      45,    0,  368,    2, 0x0a,
+      46,    1,  369,    2, 0x0a,
+      47,    1,  372,    2, 0x0a,
+      48,    1,  375,    2, 0x0a,
+      49,    1,  378,    2, 0x0a,
+      50,    1,  381,    2, 0x0a,
+      51,    0,  384,    2, 0x0a,
+      52,    1,  385,    2, 0x0a,
+      53,    1,  388,    2, 0x0a,
+      54,    1,  391,    2, 0x0a,
+      55,    1,  394,    2, 0x0a,
+      56,    1,  397,    2, 0x0a,
+      57,    1,  400,    2, 0x0a,
+      58,    1,  403,    2, 0x0a,
+      59,    0,  406,    2, 0x0a,
+      60,    1,  407,    2, 0x0a,
+      61,    0,  410,    2, 0x0a,
+      62,    1,  411,    2, 0x0a,
+      63,    1,  414,    2, 0x0a,
+      65,    1,  417,    2, 0x0a,
+      67,    4,  420,    2, 0x0a,
+      75,    1,  429,    2, 0x0a,
+      76,    1,  432,    2, 0x0a,
+      77,    1,  435,    2, 0x0a,
+      77,    0,  438,    2, 0x2a,
+      79,    1,  439,    2, 0x0a,
+
+ // slots: parameters
+    QMetaType::Void,
+    QMetaType::Void, 0x80000000 | 4,    2,
+    QMetaType::Void, QMetaType::Int,    2,
+    QMetaType::Void, QMetaType::QPoint,    7,
+    QMetaType::Void, QMetaType::Bool,    2,
+    QMetaType::Void, QMetaType::Bool,    2,
+    QMetaType::Void, QMetaType::Bool,    2,
+    QMetaType::Void, QMetaType::Bool,    2,
+    QMetaType::Void, QMetaType::Int,   13,
+    QMetaType::Void, 0x80000000 | 15,   16,
+    QMetaType::Void, 0x80000000 | 17, QMetaType::Bool,   16,   18,
+    QMetaType::Void, 0x80000000 | 17,   16,
+    QMetaType::Void,
+    QMetaType::Void, QMetaType::Double,   21,
+    QMetaType::Void, QMetaType::QString,    2,
+    QMetaType::Void, 0x80000000 | 24,   25,
+    QMetaType::Void,
+    QMetaType::Void, QMetaType::QString,    2,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void, 0x80000000 | 31, QMetaType::Int,    2,    2,
+    QMetaType::Void,
+    QMetaType::Void, 0x80000000 | 34, 0x80000000 | 36,   35,   37,
+    QMetaType::Void,
+    QMetaType::Void, QMetaType::Double,    2,
+    QMetaType::Void, QMetaType::QString,    2,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void, QMetaType::Int,    2,
+    QMetaType::Void, QMetaType::Int,    2,
+    QMetaType::Void, QMetaType::Int,    2,
+    QMetaType::Void, QMetaType::Int,    2,
+    QMetaType::Void, QMetaType::Int,    2,
+    QMetaType::Void,
+    QMetaType::Void, QMetaType::Bool,    2,
+    QMetaType::Void, QMetaType::Bool,    2,
+    QMetaType::Void, QMetaType::Bool,    2,
+    QMetaType::Void, QMetaType::Bool,    2,
+    QMetaType::Void, QMetaType::Bool,    2,
+    QMetaType::Void, QMetaType::QString,    2,
+    QMetaType::Void, QMetaType::Bool,    2,
+    QMetaType::Void,
+    QMetaType::Void, 0x80000000 | 24,   25,
+    QMetaType::Int,
+    QMetaType::Void, QMetaType::QString,   16,
+    QMetaType::Void, QMetaType::QModelIndex,   64,
+    QMetaType::Void, QMetaType::QString,   66,
+    QMetaType::Void, 0x80000000 | 68, 0x80000000 | 70, 0x80000000 | 72, 0x80000000 | 72,   69,   71,   73,   74,
+    QMetaType::Void, QMetaType::Bool,    2,
+    QMetaType::Void, QMetaType::Bool,    2,
+    QMetaType::Void, QMetaType::Bool,   78,
+    QMetaType::Void,
+    QMetaType::Void, QMetaType::QModelIndex,   64,
+
+       0        // eod
+};
+
+void WinnerWin::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+{
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        WinnerWin *_t = static_cast<WinnerWin *>(_o);
+        switch (_id) {
+        case 0: _t->DoQueryCapital(); break;
+        case 1: _t->SlotAppendLog((*reinterpret_cast< char*(*)>(_a[1]))); break;
+        case 2: _t->SlotTabChanged((*reinterpret_cast< int(*)>(_a[1]))); break;
+        case 3: _t->SlotTbvTasksContextMenu((*reinterpret_cast< QPoint(*)>(_a[1]))); break;
+        case 4: _t->SlotTbvTasksActionStart((*reinterpret_cast< bool(*)>(_a[1]))); break;
+        case 5: _t->SlotTbvTasksActionStop((*reinterpret_cast< bool(*)>(_a[1]))); break;
+        case 6: _t->SlotTbvTasksActionDel((*reinterpret_cast< bool(*)>(_a[1]))); break;
+        case 7: _t->SlotTbvTasksActionDetail((*reinterpret_cast< bool(*)>(_a[1]))); break;
+        case 8: _t->RemoveByTaskId((*reinterpret_cast< int(*)>(_a[1]))); break;
+        case 9: _t->DoStatusBar((*reinterpret_cast< const std::string(*)>(_a[1]))); break;
+        case 10: _t->DoStatusBar((*reinterpret_cast< std::string*(*)>(_a[1])),(*reinterpret_cast< bool(*)>(_a[2]))); break;
+        case 11: _t->DoStatusBar((*reinterpret_cast< std::string*(*)>(_a[1]))); break;
+        case 12: _t->DoFlashWin(); break;
+        case 13: _t->DoAlertPercentChanged((*reinterpret_cast< double(*)>(_a[1]))); break;
+        case 14: _t->DoSellTypeChanged((*reinterpret_cast< const QString(*)>(_a[1]))); break;
+        case 15: _t->ChangeTabSellAssistantImg((*reinterpret_cast< TypeTask(*)>(_a[1]))); break;
+        case 16: _t->DoLeStockEditingFinished(); break;
+        case 17: _t->DoLeStockChanged((*reinterpret_cast< const QString(*)>(_a[1]))); break;
+        case 18: _t->DoSpboxQuantityEditingFinished(); break;
+        case 19: _t->DoAddTask(); break;
+        case 20: _t->DoTaskStatChangeSignal((*reinterpret_cast< StrategyTask*(*)>(_a[1])),(*reinterpret_cast< int(*)>(_a[2]))); break;
+        case 21: _t->DoQueryPosition(); break;
+        case 22: _t->InsertIntoTbvTasklist((*reinterpret_cast< QTableView*(*)>(_a[1])),(*reinterpret_cast< T_TaskInformation(*)>(_a[2]))); break;
+        case 23: _t->ResetSellTabTaskTime(); break;
+        case 24: _t->DoBuyAlertPercentChanged((*reinterpret_cast< double(*)>(_a[1]))); break;
+        case 25: _t->DoBuyTypeChanged((*reinterpret_cast< const QString(*)>(_a[1]))); break;
+        case 26: _t->DoAddBuyTask(); break;
+        case 27: _t->DoQueryQtyCanBuy(); break;
+        case 28: _t->ResetBuyTabTaskTime(); break;
+        case 29: _t->DoAddEqSectionTask(); break;
+        case 30: _t->ResetEqSectionTaskTime(); break;
+        case 31: _t->DoMaxQtyCheckBoxChanged((*reinterpret_cast< int(*)>(_a[1]))); break;
+        case 32: _t->DoMinQtyCheckBoxChanged((*reinterpret_cast< int(*)>(_a[1]))); break;
+        case 33: _t->DoMaxStopTrigCheckBoxChanged((*reinterpret_cast< int(*)>(_a[1]))); break;
+        case 34: _t->DoMinClearTrigCheckBoxChanged((*reinterpret_cast< int(*)>(_a[1]))); break;
+        case 35: _t->DoRebounceCheckBoxChanged((*reinterpret_cast< int(*)>(_a[1]))); break;
+        case 36: _t->DoAddIndexTradeTask(); break;
+        case 37: _t->DoTrdIndexRadioCrossDownChecked((*reinterpret_cast< bool(*)>(_a[1]))); break;
+        case 38: _t->DoTrdIndexRadioCrossUpChecked((*reinterpret_cast< bool(*)>(_a[1]))); break;
+        case 39: _t->DoTrdIndexAlertBtnBtnChecked((*reinterpret_cast< bool(*)>(_a[1]))); break;
+        case 40: _t->DoTrdIndexRelBtnBtnChecked((*reinterpret_cast< bool(*)>(_a[1]))); break;
+        case 41: _t->DoTrdIndexClearBtnChecked((*reinterpret_cast< bool(*)>(_a[1]))); break;
+        case 42: _t->DoBktestTypeChanged((*reinterpret_cast< const QString(*)>(_a[1]))); break;
+        case 43: _t->DoStartBacktest((*reinterpret_cast< bool(*)>(_a[1]))); break;
+        case 44: _t->DoEnableBtnBackTest(); break;
+        case 45: _t->ChangeTabBuyAssistantImg((*reinterpret_cast< TypeTask(*)>(_a[1]))); break;
+        case 46: { int _r = _t->TbvTasksCurRowTaskId();
+            if (_a[0]) *reinterpret_cast< int*>(_a[0]) = _r; }  break;
+        case 47: _t->FlushFromStationListWidget((*reinterpret_cast< QString(*)>(_a[1]))); break;
+        case 48: _t->OnClickedListWidget((*reinterpret_cast< QModelIndex(*)>(_a[1]))); break;
+        case 49: _t->ChangeFromStationText((*reinterpret_cast< QString(*)>(_a[1]))); break;
+        case 50: _t->AssignHintListAndLineEdit((*reinterpret_cast< HintList*(*)>(_a[1])),(*reinterpret_cast< QLineEdit*(*)>(_a[2])),(*reinterpret_cast< QDoubleSpinBox*(*)>(_a[3])),(*reinterpret_cast< QDoubleSpinBox*(*)>(_a[4]))); break;
+        case 51: _t->SlotOpenCalcWin((*reinterpret_cast< bool(*)>(_a[1]))); break;
+        case 52: _t->SlotOpenAbout((*reinterpret_cast< bool(*)>(_a[1]))); break;
+        case 53: _t->TriggerFlashWinTimer((*reinterpret_cast< bool(*)>(_a[1]))); break;
+        case 54: _t->TriggerFlashWinTimer(); break;
+        case 55: _t->DoTabTasksDbClick((*reinterpret_cast< const QModelIndex(*)>(_a[1]))); break;
+        default: ;
+        }
+    }
+}
+
+const QMetaObject WinnerWin::staticMetaObject = {
+    { &QMainWindow::staticMetaObject, qt_meta_stringdata_WinnerWin.data,
+      qt_meta_data_WinnerWin,  qt_static_metacall, 0, 0}
+};
+
+
+const QMetaObject *WinnerWin::metaObject() const
+{
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->dynamicMetaObject() : &staticMetaObject;
+}
+
+void *WinnerWin::qt_metacast(const char *_clname)
+{
+    if (!_clname) return 0;
+    if (!strcmp(_clname, qt_meta_stringdata_WinnerWin.stringdata))
+        return static_cast<void*>(const_cast< WinnerWin*>(this));
+    return QMainWindow::qt_metacast(_clname);
+}
+
+int WinnerWin::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = QMainWindow::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 56)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 56;
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        if (_id < 56)
+            *reinterpret_cast<int*>(_a[0]) = -1;
+        _id -= 56;
+    }
+    return _id;
+}
+QT_END_MOC_NAMESPACE
